62b298d41725e406d77f090a9143c3e28b78bc74
amir-shawkat <amirshawkat05@gmail.com>
2026-03-20 16:12:42 +0600

    Implement Registration Page

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -12,6 +12,11 @@ import { Control } from "react-hook-form"
 import Image from "next/image"
 import PhoneInput from "react-phone-number-input"
 import { E164Number } from "libphonenumber-js/core"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import { Select, SelectValue, SelectTrigger, SelectContent } from "./ui/select"
+import { Textarea } from "./ui/textarea"
+import { Checkbox } from "./ui/checkbox"
 
 export enum FormFieldType {
   INPUT = "input",
@@ -33,12 +38,14 @@ interface CustomProps {
     iconAlt?: string,
     disabled?: boolean,
     dateFormat?: string,
-    ShowTimeSelect?: boolean,
+    showTimeSelect?: boolean,
     children?: React.ReactNode,
     renderSkeleton?: (field: any) => React.ReactNode,
 }
 
 const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+
+    
    
 
     switch (props.fieldType) {
@@ -83,6 +90,69 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
                 </div>
             )
 
+        case FormFieldType.DATE_PICKER:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    <Image src="/assets/icons/calendar.svg" alt="calendar" width={24} height={24} className="ml-2" />
+                    <FormControl>  
+                        <DatePicker selected={field.value} onChange={(date) => field.onChange(date)}
+                            dateFormat={props.dateFormat ?? 'dd/MM/yyyy' }
+                            showTimeSelect={props.showTimeSelect ?? false}
+                            timeInputLabel="Time:"
+                            wrapperClassName="date-picker"
+                            placeholderText="dd/mm/yyyy"
+                        />    
+                    </FormControl>
+                </div>
+            )
+
+        case FormFieldType.SKELETON:
+            return props.renderSkeleton ? props.renderSkeleton(field) : null;
+
+        case FormFieldType.SELECT:
+            return (
+                <FormControl>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <FormControl>
+                    <SelectTrigger className="shad-select-trigger">
+                        <SelectValue placeholder={props.placeholder} />
+                    </SelectTrigger>
+                    </FormControl>
+                    <SelectContent className="shad-select-content">
+                    {props.children}
+                    </SelectContent>
+                </Select>
+                </FormControl>
+            );
+
+        case FormFieldType.TEXTAREA:
+            return (
+                <FormControl>
+                    <Textarea
+                        placeholder={props.placeholder}
+                        {...field}
+                        className="shad-textArea"
+                        disabled={props.disabled}
+                    />
+                </FormControl>
+            );
+
+        case FormFieldType.CHECKBOX:
+            return (
+                <FormControl>
+                    <div className="flex items-center gap-4">
+                        <Checkbox
+                            id={props.name}
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                        />
+                        <label htmlFor={props.name} className="checkbox-label">
+                            {props.label}
+                        </label>
+                    </div>
+                </FormControl>
+            );
+
         default:
             break;
         
@@ -98,7 +168,7 @@ const CustomFormField = (props: CustomProps) => {
         name={name}
         render={({ field }) => (
         <FormItem className="flex-1">
-            {fieldType === FormFieldType.CHECKBOX && label &&(
+            {fieldType !== FormFieldType.CHECKBOX && label &&(
                 <FormLabel>{label}</FormLabel>
             )}
 
